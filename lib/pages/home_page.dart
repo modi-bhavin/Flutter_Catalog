2643f787fa6f5bea51194870b5ed513c49dcc3ec
modi-bhavin <bhavinmodi.official@gmail.com>
2023-01-09 23:23:43 +0530

    day 13 - json file start

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,39 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
-  final int days = 30;
-  final String name = "Bhavin";
-
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final int days = 30;
+
+  final String name = "Bhavin";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  Future<void> loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    print(productsData);
+  }
+
   Widget build(BuildContext context) {
     final dummyList = List.generate(5, (index) => CatalogModel.items[0]);
     return Scaffold(
